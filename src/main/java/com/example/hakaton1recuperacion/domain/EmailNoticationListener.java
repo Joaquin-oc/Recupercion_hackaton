772458b3cdac268ea3619f04a8bf88aa8f6f45d1
Joaquín-oc <joaquin.ocana@utec.edu.pe>
2025-05-13 17:10:23 -0500

    eventnotificationlistener

--- src/main/java/com/example/hakaton1recuperacion/domain/EmailNoticationListener.java
+++ src/main/java/com/example/hakaton1recuperacion/domain/EmailNoticationListener.java
@@ -1,4 +1,19 @@
 package com.example.hakaton1recuperacion.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailNoticationListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailNoticationListener.class);
+
+    @EventListener
+    public void handleOrderCreatedEvent(OrderCreatedEvent event) {
+        logger.info("Enviando correo a " + event.getEmail() +
+                " con orden " + event.getOrderId() +
+                " del producto: " + event.getProducts());
+    }
 }
