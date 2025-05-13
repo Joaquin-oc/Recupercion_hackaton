cfd82b1a1ae607384200a6b7878c1a6b0ebb4cd7
Ignacio Vidal <ignacio.vidal@utec.edu.pe>
2025-05-13 16:48:12 -0500

    Initial commit

---
+++ src/main/java/com/example/hakaton1recuperacion/Hakaton1RecuperacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.hakaton1recuperacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hakaton1RecuperacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hakaton1RecuperacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hakaton1recuperacion/Hakaton1RecuperacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hakaton1recuperacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hakaton1RecuperacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
